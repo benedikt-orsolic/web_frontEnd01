5fb8f23681eae40e9aae48e218f4c1a11d151cf1
benedikt-orsolic <benedikt.orsolic@gmail.com>
2021-04-22 23:27:59 +0200

    Can fetch data from api

---
+++ scripts/fetch_latest.js
@@ -0,0 +1,57 @@
+
+
+
+
+const xhttp = new XMLHttpRequest();
+
+
+xhttp.onreadystatechange = function () {
+
+    if(this.readyState == 4 && this.status == 200){
+        appendToPage(JSON.parse(this.responseText));
+    }
+}
+
+xhttp.open('GET', BASE_URL + 'movie/550?api_key=' + API_KEY, true);
+xhttp.send();
+
+
+function appendToPage(jsonData){
+    console.log(jsonData);
+
+    let node = document.createElement('article');
+    node.classList.add('movieTitle');
+    
+    let title = document.createElement('h5');
+    title.appendChild(document.createTextNode(jsonData.original_title));
+
+    let poster = createPosterNode(jsonData.poster_path);
+
+    node.appendChild(title);
+    node.appendChild(poster);
+    document.getElementById('movieList').appendChild(node);
+
+}
+
+function createPosterNode(posterPath) {
+
+    
+    
+    let poster = document.createElement('img');
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            let config = JSON.parse(this.responseText);
+            console.log(config)
+            
+    
+            poster.setAttribute('src', config.images.secure_base_url + config.images.poster_sizes[config.images.poster_sizes.length - 1] + posterPath)
+        }
+    }
+    
+    xhttp.open('GET', BASE_URL + 'configuration?api_key=' + API_KEY, true);
+    xhttp.send();
+
+    return poster;
+}
